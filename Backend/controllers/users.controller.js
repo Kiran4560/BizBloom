bb23a5fca691bf78e2b390116c061bc186848493
Kiran <kiran2022003kumari@gmail.com>
2024-06-03 18:46:11 +0530

    toggelfavmarket added

--- Backend/controllers/users.controller.js
+++ Backend/controllers/users.controller.js
@@ -1,90 +1,4 @@
-// const bcrypt = require("bcryptjs");
-// const User = require("../models/user");
 
-// //signup function
-
-// const addUser = async (req, res) => {
-//   //destructuring and storing requested data
-//   const { name, email, password } = req.body;
-
-//   //finding existing user with same email;
-//   let existingUserEmail;
-//   try {
-//     existingUserEmail = await User.findOne({ email: email });
-//   } catch (err) {
-//     console.log(err.message);
-//     res.status(500).json({ error: err.message });
-    
-//   }
-  
-
-//   //checking existing userEmail
-//   if (existingUserEmail) {
-//     console.log("A user with this email already exists");
-//     console.log(existingUserEmail);
-//     res.status(422).json({ error: "A user with this email already exists" });
-//     return;
-    
-//   }
-
-//   //finding existing user with same username;
-//   let existingUserName;
-//   try {
-//     existingUserName = await User.findOne({ username: name });
-//   } catch (err) {
-//     console.log(err.message);
-//     res.status(500).json({ error: err.message });
-//   }
-
-//   //checking existing username
-//   if (existingUserName) {
-//     console.log("A user with this username already exists");
-//     console.log(existingUserName);
-//     res.status(422).json({ error: "A user with this username already exists" });
-//     return;
-//   } 
-
-  
-//   async function hashPassword() {
-//     const saltRounds = 10; // Defines the number of salt rounds
-
-//     const hashedPassword = await new Promise((resolve, reject) => {
-//       bcrypt.hash(password, saltRounds, function (err, hash) {
-//         if (err) reject(err);
-//         resolve(hash);
-//       });
-//     });
-
-//     return hashedPassword;
-//   }
-
-//   const hashedPassword = await hashPassword();
-
-//   //creating newUser object
-//   const newUser = new User({
-//     username: name,
-//     email,
-//     password: hashedPassword,
-//     favourites: [],
-//     markets: [],
-//   });
-
-//   //add newUser to database
-//   try {
-//     await newUser.save();
-//   } catch (err) {
-//     console.log(err.message);
-//     res.status(500).json({ error: err.message });
-//   }
-
-//   console.log("User added");
-//   console.log(newUser);
-//   //resending data with 'OK' status code
-//   // res.status(201).json({ user: newUser });
-//   // return;
-
- 
-// };
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
@@ -177,59 +91,58 @@ const loginUser = async (req, res) => {
   }
 };
 
-// const loginUser = async(req,res)=>{
-//   const {email,password}=req.body;
+//add and remove fav market
+const toggleAddFavMarket = async (req, res) => {
+  const { userId, marketId } = req.body;
 
-//   let existingUser ;
-  
-//   try{
-//     existingUser = User.findOne({email:email});
-//   }catch(err){
-//     console.log(err.message);
-//     res.status(500).json({error : "Login in failed,please try again later"});
-//     return;
-//   }
+  try {
+    // Find the user by ID
+    const user = await User.findOne({ _id: userId });
 
-//   //user not found
-//   if(!existingUser){
-//     console.log("User with this email doesn't exist");
-//     res.status(401).json({error: "No user exists with this email,please signup"});
-//     return;
-//   }
+    if (!user) {
+      console.error(`Error: No user with this id exists: ${userId}`);
+      return res.status(401).json({ error: "No user with this id exists" });
+    }
 
-//   //User found then hash password
-// async function checkPassword(){
-// const isMatching = await new Promise((resolve,reject)=>{
-//   bcrypt.compare(password,existingUser.password,(error,isMatch)=>{
-//     if(error)
-//     reject(error);
-//   resolve(isMatch);
+    // Check if the market is already in the user's favorites
+    const existingMarket = user.favourites.find((mid) => {
+      console.log(mid, marketId, mid === marketId);
+      return mid === marketId;
+    });
+    console.log("Existing market id : ", existingMarket);
 
-//   });
-// });
-// return isMatching;
-//   }
-
-//   const isPasswordMatching = await checkPassword();
-//   console.log(isPasswordMatching);
-
-//   //Wrong password
-//   if(!isPasswordMatching){
-//     console.log("Wrong Password");
-//     res.status(401).json({error : "Wrong Password, Please enter the correct Password"});
-   
-//   }
-//   else{
-//     console.log("User found");
-//     console.log(existingUser);
-//     res.status(201).json({
-//       message : "Logged in!",
-//       user: existingUser,
-//     });
-//   }
-
-// };
+    if (existingMarket) {
+      // Market is already in favorites, so remove it
+      try {
+        await User.updateOne(
+          { _id: userId },
+          { $pull: { favourites: marketId } }
+        );
+        res.status(201).json({ message: "Removed from fav" });
+      } catch (updateError) {
+        console.error(`Error removing market from favorites: ${updateError.message}`);
+        res.status(500).json({ error: updateError.message });
+      }
+    } else {
+      // Market is not in favorites, so add it
+      try {
+        await User.updateOne(
+          { _id: userId },
+          { $push: { favourites: marketId } }
+        );
+        res.status(201).json({ message: "Added to fav" });
+      } catch (updateError) {
+        console.error(`Error adding market to favorites: ${updateError.message}`);
+        res.status(500).json({ error: updateError.message });
+      }
+    }
+  } catch (err) {
+    console.error(`Error finding user: ${err.message}`);
+    res.status(500).json({ error: err.message });
+  }
+};
 
 
+exports.toggleAddFavMarket = toggleAddFavMarket;
 exports.signup = addUser;
 exports.login = loginUser;
