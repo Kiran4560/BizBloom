78566217b591454b938ffb7612432415cd4bf35e
Kiran <kiran2022003kumari@gmail.com>
2024-06-04 20:37:20 +0530

    resetpassword api added /testing part not done

--- Backend/routers/user.routers.js
+++ Backend/routers/user.routers.js
@@ -4,9 +4,12 @@ const router = express.Router();
 
 const userController= require("../controllers/users.controller");
 
-//setting apis
+//user apis
 router.post("/signup",userController.signup);
 router.post("/login",userController.login);
 router.post("/togglefav", userController.toggleAddFavMarket);
 
+//password forgot apis
+router.post("/forgetpassword",userController.forgetpassword);
+router.post("/resetpassword",userController.resetPassword);
 module.exports = router ;
