1d587f663c3bc04503ff7b3f2f49d0922377de78
RGRik19 <goswami.rounak.03@gmail.com>
2024-06-17 21:14:09 +0530

    Setup ENV variables access in Vite config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": import.meta.env.VITE_PROXY_URL,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (e.g., 'development', 'production')
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        // Use the environment variable for the proxy URL
+        "/api": {
+          target: env.VITE_PROXY_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
+    define: {
+      "process.env": {
+        VITE_PROXY_URL: env.VITE_PROXY_URL,
+      },
+    },
+  };
 });
